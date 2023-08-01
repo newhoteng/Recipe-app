f334553d193f12ef71bb25ce3e9c3cce3580f53d
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-01 18:03:57 +0200

    Update route for recipe foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'user/index'
+  get 'user/show'
   resources :foods
   resources :users do
     resources :recipes do
+      resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit]
     end
   end
   root "recipes#public"
