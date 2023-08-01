1995490e26d9fe59f114fa591e28d0a8954857b2
Harriet Oteng <harriet.oteng@gmail.com>
2023-08-01 09:24:06 +0000

    Implement devise links

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   before_action :set_food, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   # GET /foods or /foods.json
   def index
@@ -19,7 +20,8 @@ class FoodsController < ApplicationController
 
   # POST /foods or /foods.json
   def create
-    @food = Food.new(food_params)
+    @user = current_user
+    @food = @user.foods.new(food_params)
 
     respond_to do |format|
       if @food.save
