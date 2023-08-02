28a810a6d576e3d0ea1b35f843fda95513ca1673
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-02 16:38:15 +0200

    Update error handling for public recipe view

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods
-  # has_many :recipe_foods, foreign_key: 'recipe_id', dependent: :destroy, class_name: 'RecipeFood'
+  has_many :recipe_foods, foreign_key: 'recipe_id', dependent: :destroy, class_name: 'RecipeFood'
 
   attribute :public, :boolean, default: false
   validates :name, :preparation_time, :cooking_time, :description, presence: true
