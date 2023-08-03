4e7a3cdbc663ef0bff0a00982053e0a2952a09e7
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-03 15:29:33 +0200

    Update for Optimization

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -3,4 +3,25 @@ class RecipeFood < ApplicationRecord
   belongs_to :food, class_name: 'Food', foreign_key: 'food_id'
 
   validates :quantity, presence: true
+
+  after_save :update_foods_counter, :update_foods_total_price
+  before_destroy :decrement_foods_counter, :reduce_foods_total_price
+
+  private
+
+  def update_foods_counter
+    recipe.increment!(:foods_counter)
+  end
+
+  def update_foods_total_price
+    recipe.increment!(:foods_total_price, (food.price * quantity))
+  end
+
+  def reduce_foods_total_price
+    recipe.decrement!(:foods_total_price, (food.price * quantity))
+  end
+
+  def decrement_foods_counter
+    recipe.decrement!(:foods_counter)
+  end
 end
