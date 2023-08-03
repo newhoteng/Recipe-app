7f35985837a4bb6d1a0e05ceb282625aabb24749
Harriet Oteng <harriet.oteng@gmail.com>
2023-08-03 00:25:59 +0000

    Fix linter errors

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  
 
   attribute :public, :boolean, default: false
   validates :name, :preparation_time, :cooking_time, :description, presence: true
