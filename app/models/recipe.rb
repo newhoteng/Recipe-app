66c705cb937f4fd7740212b9ae122d8c7e29e69f
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-03 16:23:26 +0200

    Fix issue with public view

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_foods, foreign_key: 'recipe_id', dependent: :destroy, class_name: 'RecipeFood'
   has_many :foods, foreign_key: 'recipe_id', dependent: :destroy, class_name: 'RecipeFood'
 
   attribute :public, :boolean, default: false
